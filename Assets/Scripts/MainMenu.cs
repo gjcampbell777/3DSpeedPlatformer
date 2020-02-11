48f86fff8a5fb6eed533afbe9fa1fd8fa5f6c00e
Gregory Campbell <gjcampbell777@gmail.com>
2020-02-10 22:21:40 -0500

    Filled out rest of main menu pages

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -56,48 +56,136 @@ public class MainMenu : MonoBehaviour
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(skinsFirstButton);
+    	BeanTalk.text = "WHAT DO I DO AGAIN?";
 	}
 
 	public void BackSkins()
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(skinsBackButton);
+    	switch (Random.Range(0,5))
+    	{
+    		case 0:
+    			BeanTalk.text = "THAT'S MY GAME!";
+    			break;
+    		case 1:
+    			BeanTalk.text = "GOTTA GO FAST!";
+    			break;
+    		case 2:
+    			BeanTalk.text = "IT ME!";
+    			break;
+    		case 3:
+    			BeanTalk.text = "DO YOUR BEST!";
+    			break;
+    		case 4:
+    			BeanTalk.text = "NEVER GIVE UP!";
+    			break;
+    		default:
+    			BeanTalk.text = "ERROR! SAVE ME!";
+    			break;
+    	}
 	}
 
 	public void Highscores()
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(highscoreFirstButton);
+    	BeanTalk.text = "GOOD JOB!";
 	}
 
 	public void BackHighscores()
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(highscoreBackButton);
+    	switch (Random.Range(0,5))
+    	{
+    		case 0:
+    			BeanTalk.text = "THAT'S MY GAME!";
+    			break;
+    		case 1:
+    			BeanTalk.text = "GOTTA GO FAST!";
+    			break;
+    		case 2:
+    			BeanTalk.text = "IT ME!";
+    			break;
+    		case 3:
+    			BeanTalk.text = "DO YOUR BEST!";
+    			break;
+    		case 4:
+    			BeanTalk.text = "NEVER GIVE UP!";
+    			break;
+    		default:
+    			BeanTalk.text = "ERROR! SAVE ME!";
+    			break;
+    	}
 	}
 
 	public void GameInfo()
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(gameinfoFirstButton);
+    	BeanTalk.text = "SO MANY BUTTONS!";
 	}
 
 	public void BackGameInfo()
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(gameinfoBackButton);
+    	switch (Random.Range(0,5))
+    	{
+    		case 0:
+    			BeanTalk.text = "THAT'S MY GAME!";
+    			break;
+    		case 1:
+    			BeanTalk.text = "GOTTA GO FAST!";
+    			break;
+    		case 2:
+    			BeanTalk.text = "IT ME!";
+    			break;
+    		case 3:
+    			BeanTalk.text = "DO YOUR BEST!";
+    			break;
+    		case 4:
+    			BeanTalk.text = "NEVER GIVE UP!";
+    			break;
+    		default:
+    			BeanTalk.text = "ERROR! SAVE ME!";
+    			break;
+    	}
 	}
 
 	public void Options()
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    	BeanTalk.text = "LOOK AT THOSE GRAPHIX!";
 	}
 
 	public void BackOptions()
 	{
 		EventSystem.current.SetSelectedGameObject(null);
     	EventSystem.current.SetSelectedGameObject(optionsBackButton);
+    	switch (Random.Range(0,5))
+    	{
+    		case 0:
+    			BeanTalk.text = "THAT'S MY GAME!";
+    			break;
+    		case 1:
+    			BeanTalk.text = "GOTTA GO FAST!";
+    			break;
+    		case 2:
+    			BeanTalk.text = "IT ME!";
+    			break;
+    		case 3:
+    			BeanTalk.text = "DO YOUR BEST!";
+    			break;
+    		case 4:
+    			BeanTalk.text = "NEVER GIVE UP!";
+    			break;
+    		default:
+    			BeanTalk.text = "ERROR! SAVE ME!";
+    			break;
+    	}
 	}
 
 	public void QuitGame()
